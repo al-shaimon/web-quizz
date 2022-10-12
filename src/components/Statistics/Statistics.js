c26ebe553e2f0b4faeee7011a35c9779f6d8fe5e
al-shaimon <alshaimon968@gmail.com>
2022-10-12 20:56:40 +0600

    statics added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,75 +1,44 @@
-import React, {
-    useEffect,
-    useState
-} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {
-    CartesianGrid,
-    Line,
-    LineChart,
-    Tooltip,
-    XAxis,
-    YAxis
-} from 'recharts';
-
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
-    const [course, SetCourse] = useState([]);
-    useEffect(() => {
-        axios.get('https://openapi.programming-hero.com/api/quiz')
-            .then(data => {
-                const chartcourse = data.data.data;
-                const courseData = chartcourse.map(course => {
+  const [course, SetCourse] = useState([]);
+  useEffect(() => {
+    axios.get('https://openapi.programming-hero.com/api/quiz').then((data) => {
+      const chartcourse = data.data.data;
+      const courseData = chartcourse.map((course) => {
+        const courses = {
+          name: course.name,
+          quiz: course.total,
+        };
+        return courses;
+      });
+      SetCourse(courseData);
+    });
+  }, []);
 
-                        const courses = {
-                            name: course.name,
-                            quiz: course.total
-                        }
-                        return courses;
-                    }
-
-                )
-                SetCourse(courseData)
-            })
-    }, [])
-
-    return ( <
-        div className = 'flex justify-center' >
-        <
-        LineChart width = {
-            370
-        }
-        height = {
-            300
-        }
-        data = {
-            course
-        }
-        margin = {
-            {
-                top: 2,
-                right: 10,
-                bottom: 5,
-                left: 0
-            }
-        } >
-        <
-        Line type = "monotone"
-        dataKey = "quiz"
-        stroke = "#8884d8" / >
-        <
-        CartesianGrid stroke = "#ccc"
-        strokeDasharray = "5 5" / >
-        <
-        XAxis dataKey = "name" / >
-        <
-        YAxis / >
-        <
-        Tooltip / >
-        <
-        /LineChart> <
-        /div>
-    );
+  return (
+    <div className="flex justify-center my-5 py-5">
+      <LineChart
+        width={370}
+        height={300}
+        data={course}
+        margin={{
+          top: 5,
+          right: 10,
+          bottom: 5,
+          left: 0,
+        }}
+      >
+        <Line type="monotone" dataKey="quiz" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>{' '}
+    </div>
+  );
 };
 
 export default Statistics;
